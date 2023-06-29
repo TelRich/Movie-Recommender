ffd4b39433c54b32bc4b74ae1630675d9272d3ad
TelRich <okorogoodrich@gmail.com>
2023-06-29 09:37:16 +0100

    fixed malformed node issue

--- recommender.py
+++ recommender.py
@@ -42,12 +42,12 @@ movie_lk = load_data()[4]
 movie_md = movie_md[['genres', 'id', 'imdb_id', 'release_date', 'title', 'vote_average', 'vote_count', 'popularity', 'runtime']]
 
 # extracting the contents
-movie_md['genres'] = movie_md.loc[:,'genres'].apply(lambda x: [genre['name'] for genre in ast.literal_eval(x)])
-movie_cr['cast'] = movie_cr.loc[:,'cast'].apply(lambda x: [i['name'] for i in ast.literal_eval(x)])
-movie_kw['keywords'] = movie_kw.loc[:,'keywords'].apply(lambda x: [i['name'] for i in ast.literal_eval(x)])
+movie_md['genres'] = movie_md['genres'].apply(lambda x: [genre['name'] for genre in ast.literal_eval(x)])
+# movie_cr['cast']
+# movie_kw['keywords'] = movie_kw['keywords'].apply(lambda x: [i['name'] for i in ast.literal_eval(x)])
 
 # change date data type
-movie_md['release_date'] = pd.to_datetime(movie_md.loc[:,'release_date'], errors='coerce')
+movie_md['release_date'] = pd.to_datetime(movie_md['release_date'], errors='coerce')
 # dropped invalid id
 movie_md = movie_md.drop([19730, 29503, 35587])
 movie_md['id'] = movie_md['id'].astype('int')
@@ -63,11 +63,13 @@ merged_data = movie_md.merge(movie_cr.loc[:,['cast', 'id']], on='id')\
 merged_data['content'] = merged_data['genres'] + merged_data['cast'] + merged_data['keywords']
 merged_data['content'] = merged_data['content'].apply(lambda x: ' '.join(x))
 
+sample = merged_data[:20001]
+
 count = CountVectorizer(stop_words = 'english')
-count_matrix = count.fit_transform(merged_data['content'])
+count_matrix = count.fit_transform(sample['content'])
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
 
-indices = pd.Series(merged_data.index, index=merged_data['title']).drop_duplicates()
+indices = pd.Series(sample.index, index=sample['title']).drop_duplicates()
 
 
 m = movie_md['vote_count'].quantile(0.9)
@@ -98,14 +100,14 @@ def top_x_genre_movie(data, genre='Drama', m=m, val=100):
     top_x = filterd_movie_md.sort_values('wr', ascending=False).loc[:, 'title':'wr'].head(val).reset_index(drop=True)
     return top_x
 
-@st.cache_data
-# Fucntion to return recommended movie based on content
-def recommended_movie(movie_title, top_x, cosine_sim=cosine_sim):
-    movie_index = indices[movie_title]
-    pairwise_similarity_score = sorted(list(enumerate(cosine_sim[movie_index])), key=lambda x: x[1], reverse=True)
-    top_similar_movie = pairwise_similarity_score[1:top_x]
-    recom_movie = merged_data.iloc[[x[0] for x in top_similar_movie]].reset_index(drop=True)
-    return recom_movie.iloc[:, 4:10]
+# @st.cache_data
+# # Fucntion to return recommended movie based on content
+# def recommended_movie(movie_title, top_x, cosine_sim=cosine_sim):
+#     movie_index = indices[movie_title]
+#     pairwise_similarity_score = sorted(list(enumerate(cosine_sim[movie_index])), key=lambda x: x[1], reverse=True)
+#     top_similar_movie = pairwise_similarity_score[1:top_x]
+#     recom_movie = merged_data.iloc[[x[0] for x in top_similar_movie]].reset_index(drop=True)
+#     return recom_movie.iloc[:, 4:10]
 
 
 # Hide index numbers
